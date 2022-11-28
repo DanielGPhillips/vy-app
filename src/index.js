e84a851b7a2f356d97a2ea19b6dddc94ea019ebf
Danny Phillips <danielphillips0101@gmail.com>
2022-11-27 20:02:03 -0800

    started restructuring folders and removed some of base react app items.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider, responsiveFontSizes } from '@mui/material';
+import { StyledEngineProvider } from '@mui/material/styles';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+let theme = createTheme ({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#5994ce',
+    },
+    secondary: {
+      main: '#5c58b6',
+    },
+    error: {
+      main: '#ff0053',
+    },
+    text: {
+      primary: '#e1f5fe',
+    },
+    background: {
+      default: '#131629',
+      paper: '#151c35',
+    },
+    divider: '#b957ce',
+    info: {
+      main: '#9575cd',
+    },
+  },
+  typography: {
+    fontFamily: 'koulen, cursive'
+  },
+});
+
+theme = responsiveFontSizes(theme)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </StyledEngineProvider>
   </React.StrictMode>
 );
 
---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+// Imports
+
+export default function Home() {
+    return (
+        <h1>Woah is this actually working? I sure hope so.</h1>
+    )
+}
